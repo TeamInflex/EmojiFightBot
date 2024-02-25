50723d41c2d29d488f963fbb3d7a17d393c15f1a
Team Inflex <TheTeamInflex@Gmail.com>
2024-02-25 12:57:47 +0530

    Update emojifight.py

--- emojifight.py
+++ emojifight.py
@@ -124,6 +124,12 @@ schedule.every().day.at("00:00").do(clear_top_collection)
 
 # ...
 
+# Create an instance of the Updater class
+updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
+
 # Register command handlers
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("broadcast", broadcast, pass_args=True))
